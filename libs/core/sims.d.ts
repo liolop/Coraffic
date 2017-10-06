3f2874b83953b6091b0b6a341105cc841c07406e
Yuanmeng Li <torres2192@gmail.com>
2017-10-06 10:54:29 -0400

    delete useless functions

--- libs/core/sims.d.ts
+++ libs/core/sims.d.ts
@@ -1,14 +1,4 @@
 // Auto-generated from simulator. Do not edit.
-declare namespace actions {
-    /**
-     * Run the car
-     */
-    //% weight=55
-    //% blockId=device_run block="run the car"
-    //% shim=actions::runAsync promise
-    function run(): void;
-
-}
 declare namespace console {
     /**
      * Print out message
@@ -27,61 +17,6 @@ declare namespace loops {
     //% blockId=device_forever block="forever"
     //% shim=loops::forever
     function forever(body: () => void): void;
-
-}
-    /**
-     * A Car
-     */
-    //%
-    declare class Car {
-        //%
-        //% shim=.x
-        public x: number;
-
-        //%
-        //% shim=.y
-        public y: number;
-
-        //%
-        //% shim=.angle
-        public angle: number;
-
-    }
-    //%
-    declare class TrafficLight {
-        //%
-        //% shim=.x
-        public x: number;
-
-        //%
-        //% shim=.y
-        public y: number;
-
-        //%
-        //% shim=.lightNum
-        public lightNum: number;
-
-        //%
-        //% shim=.direct
-        public direct: ColorDirect;
-
-    }
-declare namespace objects {
-    /**
-     * @param position
-     */
-    //% block="Create Car at %position" blockId=device_CarPosition
-    //% shim=objects::createCar
-    function createCar(position: CarPos): void;
-
-    /**
-     * @param lightPos
-     * @param lightDir
-     * @param direct
-     */
-    //% block="Create Traffic Light at %lightPos | face %lightDir | with %direct light" blockId=device_LightPos
-    //% shim=objects::createTrafficLightPos
-    function createTrafficLightPos(lightPos: LightPos, lightDir: LightDir, direct: ColorDirect): void;
 
 }
 
--- sim/api.ts
+++ sim/api.ts
@@ -1,14 +1,7 @@
 /// <reference path="../libs/core/enums.d.ts"/>
 
 namespace pxsim.actions {
-    /**
-     * Run the car
-     */
-    //% weight=55
-    //% blockId=device_run block="run the car"
-    export function runAsync(){
-        return board().car.runAsync();
-    }
+    
 }
 
 function logMsg(m:string) { console.log(m) }
@@ -39,104 +32,5 @@ namespace pxsim.loops {
 }
 
 namespace pxsim {
-    /**
-     * A Car
-     */
-    //%
-    export class Car{
-        //%
-        public x = 0;
-        //%
-        public y = 0;
-        //%
-        public angle = 0;
 
-        public carPosTemp: CarPos = null;
-        constructor(carPosition: CarPos){
-            this.carPosTemp = carPosition;
-            if(carPosition == CarPos.a1){
-                this.x = 20;
-                this.y = 110;
-                this.angle = 0;
-            }
-        }
-
-        public runAsync(){
-            if(this.angle == 0){
-                this.x = this.x + 5;
-                this.y = 0;
-            }
-            if(this.angle == 90){
-                this.x = 0;
-                this.y = this.y + 5;
-            }
-            if(this.x + 20 >= 190 && this.angle == 0){
-                this.angle = 0;
-                this.x = 20;
-                this.y = 110;
-            }
-            if(this.x + 20 == 110 && this.angle == 0){
-                this.angle = 90;
-                this.x = 0; 
-            }
-            board().updateCarPos(this.x,this.y,this.angle);
-            return Promise.delay(400)
-        }
-    }
-    export function initialCar(position: CarPos): Car{
-        return new Car(position);
-    }
-
-    //%
-    export class TrafficLight{
-        //%
-        public x = 0;
-        //%
-        public y = 0;
-        //%
-        public lightNum: number;
-        public lightDirNum: number;
-        //%
-        public direct: ColorDirect;
-        public lightPosTemp: LightPos;
-        public lightDirTemp: LightDir;
-        
-        constructor(lightPos: LightPos, lightDir: LightDir, direction: ColorDirect){
-            if(lightPos == LightPos.A1){
-                this.x = 110;
-                this.y = 110;
-                this.lightPosTemp = lightPos;
-            }
-            if(lightDir == LightDir.Left){
-                this.lightDirNum = 0;
-                this.lightDirTemp = lightDir;
-            }
-            if(direction == ColorDirect.Right){
-                this.lightNum = 0;
-                this.direct = direction;
-            }
-        }
-    }
-
-}
-
-namespace pxsim.objects{
-    /**
-     * @param position
-     */
-    //% block="Create Car at %position" blockId=device_CarPosition
-    export function createCar(position: CarPos){
-        board().setCarPos(position);
-    }
-
-    /**
-     * @param lightPos
-     * @param lightDir
-     * @param direct
-     */
-    //% block="Create Traffic Light at %lightPos | face %lightDir | with %direct light" blockId=device_LightPos
-    export function createTrafficLightPos(lightPos: LightPos, lightDir: LightDir, direct: ColorDirect){
-        let light = new TrafficLight(lightPos, lightDir, direct);
-        board().updateLight(light);
-    }
 }
