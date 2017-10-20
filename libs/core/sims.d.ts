068c7746d98578bd0b49fad1d4bf47485313c15d
Yuanmeng Li <torres2192@gmail.com>
2017-10-20 15:25:42 -0400

    Merging changes

--- libs/core/sims.d.ts
+++ libs/core/sims.d.ts
@@ -1,12 +1,22 @@
 // Auto-generated from simulator. Do not edit.
-declare namespace actions {
+declare namespace loops {
     /**
-     * Run the car
+     * Repeats the code forever in the background. On each iteration, allows other code to run.
+     * @param body the code to repeat
      */
-    //% weight=55
-    //% blockId=device_run block="run the car"
-    //% shim=actions::runAsync promise
-    function run(): void;
+    //% help=functions/forever weight=55 blockGap=8
+    //% blockId=device_forever block="forever"
+    //% shim=loops::forever
+    function forever(body: () => void): void;
+
+    /**
+     * Pause for the specified time in milliseconds
+     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+     */
+    //% help=functions/pause weight=54
+    //% block="pause (ms) %pause" blockId=device_pause
+    //% shim=loops::pauseAsync promise
+    function pause(ms: number): void;
 
 }
 declare namespace console {
@@ -18,70 +28,15 @@ declare namespace console {
     function log(msg: string): void;
 
 }
-declare namespace loops {
+declare namespace actions {
     /**
-     * Repeats the code forever in the background. On each iteration, allows other code to run.
-     * @param body the code to repeat
+     * Set number of cars on the map
+     * @param carNum 
      */
-    //% help=functions/forever weight=55 blockGap=8
-    //% blockId=device_forever block="forever"
-    //% shim=loops::forever
-    function forever(body: () => void): void;
-
-}
-    /**
-     * A Car
-     */
-    //%
-    declare class Car {
-        //%
-        //% shim=.x
-        public x: number;
-
-        //%
-        //% shim=.y
-        public y: number;
-
-        //%
-        //% shim=.angle
-        public angle: number;
-
-    }
-    //%
-    declare class TrafficLight {
-        //%
-        //% shim=.x
-        public x: number;
-
-        //%
-        //% shim=.y
-        public y: number;
-
-        //%
-        //% shim=.lightNum
-        public lightNum: number;
-
-        //%
-        //% shim=.direct
-        public direct: ColorDirect;
-
-    }
-declare namespace objects {
-    /**
-     * @param position
-     */
-    //% block="Create Car at %position" blockId=device_CarPosition
-    //% shim=objects::createCar
-    function createCar(position: CarPos): void;
-
-    /**
-     * @param lightPos
-     * @param lightDir
-     * @param direct
-     */
-    //% block="Create Traffic Light at %lightPos | face %lightDir | with %direct light" blockId=device_LightPos
-    //% shim=objects::createTrafficLightPos
-    function createTrafficLightPos(lightPos: LightPos, lightDir: LightDir, direct: ColorDirect): void;
+    //% help=functions/carNum weight=54
+    //% blockId=car_number block="set %carNum cars"
+    //% shim=actions::CarNum
+    function CarNum(carNum: number): void;
 
 }
 
--- sim/api.ts
+++ sim/api.ts
@@ -1,13 +1,26 @@
 /// <reference path="../libs/core/enums.d.ts"/>
 
-namespace pxsim.actions {
+
+namespace pxsim.loops {
+
     /**
-     * Run the car
+     * Repeats the code forever in the background. On each iteration, allows other code to run.
+     * @param body the code to repeat
      */
-    //% weight=55
-    //% blockId=device_run block="run the car"
-    export function runAsync(){
-        return board().car.runAsync();
+    //% help=functions/forever weight=55 blockGap=8
+    //% blockId=device_forever block="forever" 
+    export function forever(body: RefAction): void {
+        thread.forever(body)
+    }
+
+    /**
+     * Pause for the specified time in milliseconds
+     * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+     */
+    //% help=functions/pause weight=54
+    //% block="pause (ms) %pause" blockId=device_pause
+    export function pauseAsync(ms: number) {
+        return Promise.delay(ms)
     }
 }
 
@@ -19,124 +32,26 @@ namespace pxsim.console {
      */
     //% 
     export function log(msg:string) {
-        logMsg("CONSOLE: " + msg);
+        logMsg("CONSOLE: " + msg)
         // why doesn't that work?
-        board().writeSerial(msg + "\n");
+        //board().writeSerial(msg + "\n")
     }
 }
 
-namespace pxsim.loops {
+namespace pxsim.actions {
 
     /**
-     * Repeats the code forever in the background. On each iteration, allows other code to run.
-     * @param body the code to repeat
+     * Set number of cars on the map
+     * @param carNum 
      */
-    //% help=functions/forever weight=55 blockGap=8
-    //% blockId=device_forever block="forever" 
-    export function forever(body: RefAction){
-        thread.forever(body)
+    //% help=functions/carNum weight=54
+    //% blockId=car_number block="set %carNum cars"
+    export function CarNum(carNum: number){
+        let b = board();
+        b.setCarNum(carNum);
     }
 }
 
 namespace pxsim {
-    /**
-     * A Car
-     */
-    //%
-    export class Car{
-        //%
-        public x = 0;
-        //%
-        public y = 0;
-        //%
-        public angle = 0;
-
-        public carPosTemp: CarPos = null;
-        constructor(carPosition: CarPos){
-            this.carPosTemp = carPosition;
-            if(carPosition == CarPos.a1){
-                this.x = 20;
-                this.y = 110;
-                this.angle = 0;
-            }
-        }
-
-        public runAsync(){
-            if(this.angle == 0){
-                this.x = this.x + 5;
-                this.y = 0;
-            }
-            if(this.angle == 90){
-                this.x = 0;
-                this.y = this.y + 5;
-            }
-            if(this.x + 20 >= 190 && this.angle == 0){
-                this.angle = 0;
-                this.x = 20;
-                this.y = 110;
-            }
-            if(this.x + 20 == 110 && this.angle == 0){
-                this.angle = 90;
-                this.x = 0; 
-            }
-            board().updateCarPos(this.x,this.y,this.angle);
-            return Promise.delay(400)
-        }
-    }
-    export function initialCar(position: CarPos): Car{
-        return new Car(position);
-    }
-
-    //%
-    export class TrafficLight{
-        //%
-        public x = 0;
-        //%
-        public y = 0;
-        //%
-        public lightNum: number;
-        public lightDirNum: number;
-        //%
-        public direct: ColorDirect;
-        public lightPosTemp: LightPos;
-        public lightDirTemp: LightDir;
-        
-        constructor(lightPos: LightPos, lightDir: LightDir, direction: ColorDirect){
-            if(lightPos == LightPos.A1){
-                this.x = 110;
-                this.y = 110;
-                this.lightPosTemp = lightPos;
-            }
-            if(lightDir == LightDir.Left){
-                this.lightDirNum = 0;
-                this.lightDirTemp = lightDir;
-            }
-            if(direction == ColorDirect.Right){
-                this.lightNum = 0;
-                this.direct = direction;
-            }
-        }
-    }
-
-}
-
-namespace pxsim.objects{
-    /**
-     * @param position
-     */
-    //% block="Create Car at %position" blockId=device_CarPosition
-    export function createCar(position: CarPos){
-        board().setCarPos(position);
-    }
-
-    /**
-     * @param lightPos
-     * @param lightDir
-     * @param direct
-     */
-    //% block="Create Traffic Light at %lightPos | face %lightDir | with %direct light" blockId=device_LightPos
-    export function createTrafficLightPos(lightPos: LightPos, lightDir: LightDir, direct: ColorDirect){
-        let light = new TrafficLight(lightPos, lightDir, direct);
-        board().updateLight(light);
-    }
+    
 }
