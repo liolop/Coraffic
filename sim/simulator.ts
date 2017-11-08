31a74a8e8e8178cda0e16b9d9f92e493b7583587
Yuanmeng Li <liolop@qq.com>
2017-11-08 01:54:45 -0500

    eliminate unuseful function

--- sim/simulator.ts
+++ sim/simulator.ts
@@ -817,24 +817,6 @@ namespace jsLib{
         c.drawCar();
       }
     }
-  
-    // Object.getPrototypeOf(this.ctx).rounded_rect = function(x:any,y:any,w:any,h:any,r:any){
-    //   if (typeof r === "undefined") {
-    //     r = 2;
-    //   }
-    //   this.beginPath();
-    //   this.moveTo(x + r, y);
-    //   this.lineTo(x + w - r, y);
-    //   this.quadraticCurveTo(x + w, y, x + w, y + r);
-    //   this.lineTo(x + w, y + h - r);
-    //   this.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
-    //   this.lineTo(x + r, y + h);
-    //   this.quadraticCurveTo(x, y + h, x, y + h - r);
-    //   this.lineTo(x, y + r);
-    //   this.quadraticCurveTo(x, y, x + r, y);
-    //   this.closePath();
-    //   this.fill();
-    // }   
     
     intersections(): any{
       var index = 0;
